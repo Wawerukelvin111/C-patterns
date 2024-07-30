a3cbb366b95dbd36ee6fe2b82dd159859af252c1
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-07-30 20:09:22 +0000

    pattern 12
    output:
    1 10 11 20 21
      2  9 12 19 22
      3  8 13 18 23
      4  7 14 17 24
      5  6 15 16 25

---
+++ num pattern 12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,x,n=5,y;
+	for(i=1;i<=n;i++){
+		x=i;
+		y=n-i+1;
+		for(j=1;j<=n;j++){
+			if(j%2==1){
+				printf("%3d",x);
+			}
+			else{
+				printf("%3d",y);
+			}
+			x+=n;
+			y+=n;
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
