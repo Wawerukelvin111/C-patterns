c54c327a64f0b6fae22915d3e1ff5f2ebe5421a3
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-02 20:36:34 +0000

    pattern 50
    output:
      1
      9  2
     10  8  3
     14 11  7  4
     15 13 12  6  5

---
+++ num pattern 50.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,j,t1,t2,r1,r2,x=1,y=5;
+	for(i=5;i>=1;i--){
+        t1=x;
+        t2=y;
+        r1=i;
+        r2=i+1;
+		for(j=5;j>=i;j--){
+            if((i+j)%2==0){
+                printf("%3d",t1);
+            }
+			else{
+                printf("%3d",t2);
+            }
+            t1=t1-r1++;
+            t2=t2-r2++;
+		}
+        y=y+i-1;
+        x=x+i;	
+		printf("\n");
+	}
+	return 0;
+}
