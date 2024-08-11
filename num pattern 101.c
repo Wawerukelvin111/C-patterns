233f458cd13402b8bc9af656222c61feb5fe714d
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-11 21:05:24 +0300

    pattern 101
    output:
        2
       46
      81012
     14161820
    2224262830

---
+++ num pattern 101.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i,j,x=2;
+    for(i=5;i>=1;i--){
+        for(j=1;j<=5;j++){
+            if(i<=j){
+                printf("%d",x);
+                x+=2;
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
