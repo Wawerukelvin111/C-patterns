13fea59763b43e65528b9fa79aba8e6d6c9d38e0
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-17 19:38:09 +0300

    pattern 110
          5
         6 4
       12 7 3
      1311 8 2
     151410 9 1

---
+++ num pattern 110.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int n=5,x=1,y=n;
+    int i,j,s,t1,t2,r1,r2;
+    for(i=n;i>=1;i--){
+        t1=x;
+        t2=y;
+        r1=i;
+        r2=i+1;
+        for(s=1;s<=i;s++){
+            printf(" ");
+        }
+        for(j=n;j>=i;j--){
+            if((i+j)%2==1){
+                printf("%2d",t1);
+            }
+            else{
+                printf("%2d",t2);
+            }
+            t1-=r1++;
+            t2-=r2++;
+        }         
+        y+=(i-1);
+        x+=i;
+        printf("\n");
+    }
+    return 0;
+}
