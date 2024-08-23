dafe56d1244ddb258b8dc905e91e59775448e588
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-23 14:49:19 +0300

    pattern 134
    output:
    * * * * * 
     * * * * 
      * * * 
       * * 
        *

---
+++ num pattern 134.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int i,j,k;
+    int n=5;
+    for(i=n;i>=1;i--){
+        for(k=n-1;k>=i;k--){
+            printf(" ");
+        }
+        for(j=i;j>=1;j--){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
