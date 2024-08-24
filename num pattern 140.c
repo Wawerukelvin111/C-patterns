51f3435d6257bf5ac9c78e07506e4d0e5c8f28c3
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-24 14:22:29 +0300

    pattern 140
    output:
        1
       222
      33333
     4444444
    555555555

---
+++ num pattern 140.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n=5;
+    int z=1;
+    int i,j,k;
+    for(i=1;i<=n;i++){
+        for(j=n-1;j>=i;j--){
+            printf(" ");
+        }
+        for(k=0;k<z;k++){
+            printf("%d",i);
+        }
+        z+=2;
+        printf("\n");
+    }
+    return 0;
+}
