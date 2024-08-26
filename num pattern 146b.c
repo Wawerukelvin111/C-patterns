169824ec4b2ef5980fdf59e185fa1be412502934
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-08-26 21:56:39 +0300

    alternate pattern 146
    output:
        5
       654
      76543
     8765432
    987654321

---
+++ num pattern 146b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n=5;
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n*2;j++){
+            if(j>=n-i+1 && j<=n+i-1){
+                printf("%d",n*2-j);
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
