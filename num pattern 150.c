dfe9fe5792ee080b61fa5fe39eeb506ca7f5a3f2
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-09-08 11:44:52 +0300

    pattern 150
    output:
       1
      121
     12421
    1248421
    1248168421

---
+++ num pattern 150.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int n=5;
+    int i,j;
+    int p=1,c=1;
+    for(i=1;i<=n;i++){
+        for(j=n-1;j>i;j--){
+            printf(" ");
+        }
+        p=1;
+        printf("%d",p);
+        while(c<i){
+            printf("%d",p*=2);
+            c++;
+        }
+        while (c>1){
+            printf("%d",p/=2);
+            c--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
