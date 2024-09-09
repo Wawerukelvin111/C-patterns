274974d8052aa7ce87bd8ce8fafe9147b6b23b02
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-09-09 22:26:23 +0300

    pattern 155
    output:
        A
       BBB
      CCCCC
     DDDDDDD
    EEEEEEEEE

---
+++ num pattern 155.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int m=1;
+    int p_height=5;
+    int p_spaces=p_height-1;
+    int i,j,k;
+    for(i=0;i<p_height;i++){
+        for(j=p_spaces;j>i;j--){
+            printf(" ");
+        }
+        for(k=0;k<m;k++){
+            printf("%c",i+65);
+        }
+        m+=2;
+        printf("\n");
+    }
+}
