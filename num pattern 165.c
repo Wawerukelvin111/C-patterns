bb9b0116b37187cc6184fc690b8b366f27ccdab2
BlueQuay <146199308+Wawerukelvin111@users.noreply.github.com>
2024-09-11 22:09:08 +0300

    pattern 165
    output:
    123456789
     1234567
      12345
       123
        1

---
+++ num pattern 165.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int p_height=5;
+    int width=p_height*2-1;
+    int p_space=p_height-1;
+    int i,j,k;
+    for(i=p_height;i>=1;i--){
+        for(j=p_space;j>=i;j--){
+            printf(" ");
+        }
+        for(k=1;k<=width;k++){
+            printf("%d",k);
+        }
+        width-=2;
+        printf("\n");
+    } 
+    return 0;
+}
